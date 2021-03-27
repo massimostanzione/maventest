f205a843db802787f3ad27dc34b247f97301ecbd
massimostanzione <massimo.stanzione@hotmail.it>
2021-03-27 17:16:14 +0100

    Versioning into GitHub

---
+++ src/main/java/it/uniroma2/isw2/massimostanzione/ManualClass.java
@@ -0,0 +1,7 @@
+package it.uniroma2.isw2.massimostanzione;
+
+public class ManualClass {
+	public static void main(String[] args) {
+	       System.out.println("Playing with Maven");
+	}
+}
---
+++ src/test/java/it/uniroma2/isw2/massimostanzione/AppTest.java
@@ -0,0 +1,20 @@
+package it.uniroma2.isw2.massimostanzione;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
